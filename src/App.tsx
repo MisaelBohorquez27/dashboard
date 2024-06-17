a5b23721b766c49c5424e4c3ad19ff06e4cbcaa7
MisaelBohorquez27 <67847941+MisaelBohorquez27@users.noreply.github.com>
2024-06-17 17:51:39 -0500

    arreglando

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import Grid from "@mui/material/Unstable_Grid2"; // Grid version 2
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
     <Grid container spacing={5}>
