1398fc2ae057a8f5353791d8d5aebd1ea52aa29f
MisaelBohorquez27 <67847941+MisaelBohorquez27@users.noreply.github.com>
2024-06-19 14:26:52 -0500

    Agregando funcionalidades

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import "./App.css";
 import Grid from "@mui/material/Unstable_Grid2"; // Grid version 2
+import Indicator from './components/Indicator';
+import Summary from './components/Summary';
+import BasicTable from './components/BasicTable';
+
+
 
 function App() {
 
   return (
+
     <Grid container spacing={5}>
       <Grid xs={12} sm={4} md={3} lg={2}>1</Grid>
       <Grid xs={6} sm={4} md={3} lg={2}>2</Grid>
@@ -11,6 +17,16 @@ function App() {
       <Grid xs={12} sm={4} md={3} lg={2}>4</Grid>
       <Grid xs={6} sm={4} md={6} lg={2}>5</Grid>
       <Grid xs={6} sm={4} md={6} lg={2}>6</Grid>
+      <Grid xs={6} md={4} lg={2}>
+      <Indicator title='Precipitación' subtitle='Probabilidad' value={0.13} />
+      </Grid>
+      <Grid xs={6} sm={4} md={3} lg={2}>
+	    <Summary></Summary>
+      </Grid>
+      <Grid xs={12} md={6} lg={9} >
+	       <BasicTable />
+	    </Grid>        
+
     </Grid>
   );
 }
