774783e21e0bf4466ad688eac0ad1c5b220c505c
MisaelBohorquez27 <67847941+MisaelBohorquez27@users.noreply.github.com>
2024-06-24 15:28:55 -0500

    agregando mas cosas

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,8 @@ import Grid from "@mui/material/Unstable_Grid2"; // Grid version 2
 import Indicator from './components/Indicator';
 import Summary from './components/Summary';
 import BasicTable from './components/BasicTable';
-
+import WeatherChart from './components/WeatherChart';
+import ControlPanel from './components/ControlPanel';
 
 
 function App() {
@@ -18,15 +19,20 @@ function App() {
       <Grid xs={6} sm={4} md={6} lg={2}>5</Grid>
       <Grid xs={6} sm={4} md={6} lg={2}>6</Grid>
       <Grid xs={6} md={4} lg={2}>
-      <Indicator title='Precipitación' subtitle='Probabilidad' value={0.13} />
+        <Indicator title='Precipitación' subtitle='Probabilidad' value={0.13} />
       </Grid>
       <Grid xs={6} sm={4} md={3} lg={2}>
-	    <Summary></Summary>
+        <Summary></Summary>
       </Grid>
       <Grid xs={12} md={6} lg={9} >
-	       <BasicTable />
-	    </Grid>        
-
+        <BasicTable />
+      </Grid>
+      <Grid xs={12} lg={2}>
+        <ControlPanel />
+      </Grid>
+      <Grid xs={12} lg={10}>
+        <WeatherChart></WeatherChart>
+      </Grid>
     </Grid>
   );
 }
